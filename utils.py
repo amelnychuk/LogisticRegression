9a5f4a4e67817d23374539ca460e8e2911b4e796
Andrew <amelnychukoseen@gmail.com>
2018-08-15 18:58:34 -0700

    added getImageData and getBinaryData functions

--- utils.py
+++ utils.py
@@ -4,7 +4,7 @@ import pandas as pd
 
 
 def getData(balance_ones=False):
-
+    #view the class imbalance
     Y = []
     X = []
 
@@ -21,10 +21,36 @@ def getData(balance_ones=False):
     Y = np.array(Y)
 
     if balance_ones:
-        X0, Y0 = X[Y!=1, :], Y[Y!=1]
+        X0, Y0 = X[Y !=1, :], Y[Y!=1]
         X1 = X[Y==1, :]
         X1 = np.repeat(X1, 9, axis=0)
         X = np.vstack([X0, X1])
         Y = np.concatenate((Y0, [1]*len(X1)))
 
     return X, Y
+
+
+def getImageData():
+    X , Y = getData()
+    N, D = X.shape
+    d = int(np.sqrt(D))
+    X = X.reshape(N, 1, d, d)
+    return X, Y
+
+
+def getBinaryData():
+    #view the class imbalance
+    Y = []
+    X = []
+
+    first = True
+    for line in open('fer2013.csv'):
+        if first:
+            first = False
+        else:
+            row = line.split(',')
+            y = int(row[0])
+            if y == 0 or y ==1:
+                Y.append(y)
+                X.append([int(p) for p in row[1].split()])
+    return np.array(X) / 255.0, np.array(Y)
